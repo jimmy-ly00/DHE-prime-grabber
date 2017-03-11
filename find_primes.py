63947bd3d73e134697d6ab0a0cbe7f2704ff6154
Jimmy <jimmy.ly08@gmail.com>
2017-03-11 22:51:20 +0000

    Add files via upload

---
+++ find_primes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import optparse, sys, os, subprocess, re, csv
+
+with open('alexa_top1mil', 'r') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	for row in reader:
+		#define columns
+		server = row[1]
+		
+		try:
+			cmd = subprocess.check_output([os.path.dirname(sys.argv[0])+"/openssl-trace",
+				"s_client", "-trace",
+				"-cipher", "DHE",
+				"-connect", server+":443"],
+				stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=1)
+			for line in cmd.decode("utf-8").splitlines():
+				if 'dh_p' in line:
+					prime = int(re.sub(".*: ", "", line), 16)
+					print('{}, {}'.format(server, prime))
+
+		except subprocess.CalledProcessError:
+			#print('{}, {}'.format(server, "No DHE"))
+			pass
+		except subprocess.TimeoutExpired:
+		  	#print('{}, {}'.format(server, "Can't connect"))
+			pass
